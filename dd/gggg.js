9a0c399de4b13589e003bc8edd937880faf6c9f6
miss-king-cong <101532148+miss-king-cong@users.noreply.github.com>
2023-08-09 15:01:20 +0800

    Update gggg.js

--- dd/gggg.js
+++ dd/gggg.js
@@ -1,3 +1,18 @@
+/*
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+============Quantumultx===============
+[task_local]
+#美团
+00 00 7,9,11 * * * jd_speed_sign.js, tag=美团, img-url=https://raw.githubusercontent.com/Orz-3/task/master/jd.png, enabled=true
+
+================Loon==============
 [Script]
-cron "48 8,12,18 * * *" script-path=https://git.lspsp.workers.dev/https://raw.githubusercontent.com/miss-king-cong/coing/main/dd/ddgy.js,tag=美团
+cron "00 00 7,9,11 * * *" script-path=jd_speed_sign.js,tag=美团
+
+===============Surge=================
+美团 = type=cron,cronexp="00 00 7,9,11 * * *",wake-system=1,timeout=33600,script-path=jd_speed_sign.js
+
+============小火箭=========
+美团 = type=cron,script-path=jd_speed_sign.js, cronexpr="00 00 7,9,11 * * *", timeout=33600, enable=true
+*/
 console.log(123)
